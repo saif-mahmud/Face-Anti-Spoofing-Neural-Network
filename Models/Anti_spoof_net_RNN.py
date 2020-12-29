d47fbf25c85306b4d4d045cbcb29074d1cd7eb22
saif-mahmud <saif.dhrubo@gmail.com>
2020-12-29 16:51:12 +0600

    feat: Update RNN for rppg supervision

--- Models/Anti_spoof_net_RNN.py
+++ Models/Anti_spoof_net_RNN.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 torch.autograd.set_detect_anomaly(True)
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Anti_spoof_net_RNN(nn.Module):
@@ -15,14 +15,14 @@ class Anti_spoof_net_RNN(nn.Module):
         self.num_layers = 1
 
         self.LSTM = nn.LSTM(input_size=self.input_dim, hidden_size=self.hidden_dim, num_layers=self.num_layers)
-        self.fc = nn.Linear(self.hidden_dim, 2)
+        self.fc = nn.Linear(self.hidden_dim, 50)
 
     def forward(self, F, hidden):
         batch_size, _, _, _ = F.size()
         F = F.view(batch_size, 1, -1)
 
-        lstm_out, hidden = self.LSTM(F, hidden)  # lstm_out[5,1,100]
-        R = self.fc(lstm_out)  # F[5,1,2]
-        R = torch.fft(R, signal_ndim=1, normalized=False)
+        lstm_out, hidden = self.LSTM(F, hidden)  # lstm_out: [batch_size, 1, 100]
+        R = self.fc(lstm_out)  # R: [batch_size, 1, 50]
+        R = torch.fft(R, signal_ndim=1, normalized=True)
 
-        return R, hidden  # F[5,1,2]
+        return R, hidden
